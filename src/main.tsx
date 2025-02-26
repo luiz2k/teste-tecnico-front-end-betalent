9fa9201f592960fe121f5bc4376f3c48c8617445
luiz2k <luizteles934@gmail.com>
2025-02-26 17:26:01 -0300

    feat: Exibir cabeçalho e tabela de funcionários na interface

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import "./styles/globals.module.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.tsx";
+import { Header } from "./components/Header/Header.tsx";
+import { EmployeesPage } from "./pages/EmployeesPage/EmployeesPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Header />
+    <EmployeesPage />
   </StrictMode>,
 );
